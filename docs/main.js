0b621c38b464e46be79b48753fb55da53c2c7f26
Alex123-G <alexgallozo521@gmial.com>
2021-09-12 16:32:02 -0500

    asd

--- docs/main.js
+++ docs/main.js
@@ -3,15 +3,20 @@ const nav__header = document.querySelector(".nav__header");
 const nav__header_visible = document.querySelector(".nav__header_visible");
 const a__nav = document.querySelectorAll(".a__nav");
 const a__nav_length = document.querySelectorAll(".a__nav").length;
+const logo__nav = document.querySelector(`.logo__nav`);
 
 nav__toggle.addEventListener("click", () => {
-  nav__header.classList.toggle("nav__header_visible");
+	nav__header.classList.toggle("nav__header_visible");
 });
 
-a__nav.forEach((item) => {
-  item.addEventListener("click", () => {
-    nav__header.classList.toggle("nav__header_visible");
-  });
+logo__nav.addEventListener(`click`, () => {
+	nav__header.classList.remove("nav__header_visible");
+});
+
+a__nav.forEach(item => {
+	item.addEventListener("click", () => {
+		nav__header.classList.toggle("nav__header_visible");
+	});
 });
 
 // for (let index = 0; index < a__nav_length; index++) {
