2f3489f736ad812de4971ca1a3c14e328dc37190
Alex123-G <alexgallozo521@gmial.com>
2021-10-14 21:59:28 -0500

    Cambios de estilos

--- docs/main.js
+++ docs/main.js
@@ -2,25 +2,39 @@ const nav__toggle = document.querySelector(".nav__toggle");
 const nav__header = document.querySelector(".nav__header");
 const nav__header_visible = document.querySelector(".nav__header_visible");
 const a__nav = document.querySelectorAll(".a__nav");
-// const a__nav_length = document.querySelectorAll(".a__nav").length;
 const logo__nav = document.querySelector(`.logo__nav`);
 
-nav__toggle.addEventListener("click", () => {
-	nav__header.classList.toggle("nav__header_visible");
-});
-
-logo__nav.addEventListener(`click`, () => {
-	nav__header.classList.remove("nav__header_visible");
-});
-
-a__nav.forEach(item => {
-	item.addEventListener("click", () => {
+document.addEventListener("DOMContentLoaded", () => {
+	// Cuando hagamos click al toggle desaparesca
+	nav__toggle.addEventListener("click", () => {
 		nav__header.classList.toggle("nav__header_visible");
+	});
+
+	// Cuando seleccionemos el logo de inicio se cierre el toggle
+	logo__nav.addEventListener(`click`, () => {
+		nav__header.classList.remove("nav__header_visible");
+	});
+
+	// Recorriendo los a_nav para cuando agamos click a alguno se oculte.
+	a__nav.forEach(item => {
+		item.addEventListener("click", () => {
+			nav__header.classList.toggle("nav__header_visible");
+		});
+	});
+
+	document.addEventListener("click", e => {
+		if (e.target.matches(".portafolio-container-card *")) {
+			e.target
+				.closest(`.portafolio-container-card`)
+				.querySelector(`.modal_visible`)
+				.classList.toggle("modal_invisible");
+		}
 	});
 });
 
+// Efecto Scrooll
 ScrollReveal({ distance: "100px" });
-ScrollReveal().reveal(".inicio__div", { duration: 1600 }, { origin: "top" });
-ScrollReveal().reveal(".scroll_sobremo", { duration: 1600 }, { origin: "bottom" });
-ScrollReveal().reveal(".scrollportafolio", { duration: 1600 }, { origin: "top" });
-ScrollReveal().reveal("#contactame", { duration: 1600 }, { origin: "bottom" });
+ScrollReveal().reveal(".inicio__div", { duration: 1500 }, { origin: "top" });
+ScrollReveal().reveal(".scroll_sobremo", { duration: 1500 }, { origin: "bottom" });
+ScrollReveal().reveal(".scrollportafolio", { duration: 1500 }, { origin: "top" });
+ScrollReveal().reveal("#contactame", { duration: 1500 }, { origin: "bottom" });
